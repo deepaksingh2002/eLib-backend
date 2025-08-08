1b225ed27c5318f2012bc3f2c4d5da42282bc491
Deepak Singh <deepakksingh1202@gmail.com>
2025-08-09 00:20:35 +0530

    add errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express from "express";
 
+import errorHandler from "./middlewares/errorHandler";
+
+
+
 const app = express()
 
-//routes
+// routes
+
+// Http methods GET, POST PUT PATCH DELETE
 
 app.get('/', (req, res, next) => {
+    
     res.json({message:"Welcome on eBook"})
 })
+
+
+// globle error handler
+
+app.use(errorHandler)
 
 
 export default app
