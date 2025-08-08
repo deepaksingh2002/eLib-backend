ffd32cb7a4227604e3b8adecc538e5d44c8ad664
Deepak Singh <deepakksingh1202@gmail.com>
2025-08-08 14:15:04 +0530

    add database setup.

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+import { configDotenv } from "dotenv"
+
+
+configDotenv();
+
+const _config = {
+
+    port: process.env.PORT,
+    databaseUrl: process.env.MONGO_CONNECTION_STRING,
+    
+}
+
+export const config = Object.freeze(_config)
