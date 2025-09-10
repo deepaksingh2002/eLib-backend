a3f0c87d6c8be7407e0df00678a06345bb184458
Deepak Singh <deepakksingh1202@gmail.com>
2025-09-11 01:08:42 +0530

    add login controler

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -6,6 +6,7 @@ import {sign} from "jsonwebtoken";
 import { config } from "../config/config";
 import { User } from "./userTypes";
 
+
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const {name, email, password} = req.body;
 
@@ -52,6 +53,45 @@ try {
 } catch (err) {
     return next(createHttpError(500, "Error while signing the jwt token"))
 }
-}
+};
 
-export {createUser}
+const loginUser = async(req: Request, res: Response, next: NextFunction) => {
+
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        const error = createHttpError(400, "All fields are required!");
+        return next(error);
+    }
+
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            const error = createHttpError(404, "User not found");
+            return next(error);
+        }
+    
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return next(createHttpError(400, "email or password incorrect."));
+        }
+
+   
+        const token = sign({sub: user._id}, config.jwtSecret as string, {
+            expiresIn: "7d", 
+            algorithm: "HS256"
+        })
+        res.json(
+            {
+                accesToken: token
+            }
+        )
+    } catch (err) {
+        return next(createHttpError(500, "Error while signing the jwt token"))
+    }
+};
+
+
+
+export {createUser, loginUser}
